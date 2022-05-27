27e4d3d9a565e48d3c49d80ce489c135c93ebd91
HeleneVaessen <helene2k@live.nl>
2022-05-27 22:34:44 +0200

    Added consul

---
+++ Shared/Consul/ServiceDiscoveryExt.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Shared.Consul
+{
+    public static class ServiceDiscoveryExt
+    {
+        public static void RegisterConsulServices(this IServiceCollection services, ServiceConfig serviceConfig)
+        {
+            if (serviceConfig == null)
+            {
+                throw new ArgumentNullException(nameof(serviceConfig));
+            }
+
+            var consulClient = CreateConsulClient(serviceConfig);
+
+            services.AddSingleton(serviceConfig);
+            services.AddSingleton<IHostedService, ServiceDiscoveryHostedService>();
+            services.AddSingleton<IConsulClient, ConsulClient>(p => consulClient);
+        }
+
+        private static ConsulClient CreateConsulClient(ServiceConfig serviceConfig)
+        {
+            return new ConsulClient(config =>
+            {
+                config.Address = serviceConfig.ServiceDiscoveryAddress;
+            });
+        }
+    }
+}
