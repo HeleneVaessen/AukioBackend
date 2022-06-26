9a91a2e946ef5b41ddd9bd44d64fad0a4619bc26
HeleneVaessen <helene2k@live.nl>
2022-06-26 22:39:58 +0200

    Added unit tests for summary service

---
+++ SummaryService.Test/MockSummaryRepository.cs
@@ -0,0 +1,25 @@
+﻿using SummaryService.DAL;
+using SummaryService.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummaryService.Test
+{
+    public class MockSummaryRepository : ISummaryRepository
+    {
+        private List<Summary> summaries = new List<Summary>();
+        public List<Summary> GetSummaries()
+        {
+            return summaries;
+        }
+
+        public Task Save(Summary summary)
+        {
+            summaries.Add(summary);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ SummaryService.Test/Tests/UnitTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using SummaryService.DAL;
+using SummaryService.Models;
+using SummaryService.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SummaryService.Test.Tests
+{
+    class UnitTests
+    {
+        private ISummaryService _summaryService;
+        private ISummaryRepository _summaryRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _summaryRepository = new MockSummaryRepository();
+            _summaryService = new SummaryService.Services.SummaryService(_summaryRepository);
+        }
+
+        [Test]
+        public void AddSummaryTest()
+        {
+            Summary summary = new Summary()
+            {
+                Content = "Content",
+                Id = "",
+                SummaryID = Guid.NewGuid(),
+                Title = "Title",
+                UserId = 2
+            };
+
+            _summaryService.PostSummary(summary);
+
+            Assert.AreEqual(_summaryRepository.GetSummaries().Find(e => e.UserId == 2), summary); 
+        }
+
+        [Test]
+        public void GetSummariesTest()
+        {
+            Summary summary1 = new Summary()
+            {
+                Content = "Content",
+                Id = "",
+                SummaryID = Guid.NewGuid(),
+                Title = "Title",
+                UserId = 2
+            };
+
+            Summary summary2 = new Summary()
+            {
+                Content = "Content",
+                Id = "",
+                SummaryID = Guid.NewGuid(),
+                Title = "Title",
+                UserId = 3
+            };
+
+            _summaryService.PostSummary(summary1);
+            _summaryService.PostSummary(summary2);
+
+            Assert.AreEqual(_summaryRepository.GetSummaries().Count, 2);
+            Assert.AreEqual(_summaryRepository.GetSummaries().Find(e => e.UserId == 2), summary1);
+            Assert.AreEqual(_summaryRepository.GetSummaries().Find(e => e.UserId == 3), summary2);
+        }
+    }
+}
